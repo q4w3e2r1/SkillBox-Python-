a9b6a6dfce54144692e3c40757cf5a356e6c8e47
q4w3e2r1 <126683181+q4w3e2r1@users.noreply.github.com>
2023-10-18 17:27:35 +0500

    Add files via upload

---
+++ buns/mod4/task4.py
@@ -0,0 +1,25 @@
+word = input()
+possible_to_do = True
+result = ""
+if len(word) % 2 == 0:
+    for i in word:
+        if word.count(i) % 2 == 1:
+            checker = False
+            break
+else:
+    countCheck = 0
+    for i in word:
+        if word.count(i) % 2 == 1:
+            countCheck += 1
+            result += i
+        if countCheck > 1:
+            checker = False
+            break
+if possible_to_do:
+    for i in set(word):
+        result += i * (word.count(i) // 2)
+    if len(word) % 2 == 1:
+        result = result[::-1] + result[1::]
+    else:
+        result += result[::-1]
+    print(result)
